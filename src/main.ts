6606b40f8887f55a691f32f99695e3b3959b6c18
DMarjar <20213tn119@utez.edu.mx>
2024-07-05 16:55:12 -0600

    [ADD] project structure and basic dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueSweetalert2 from "vue-sweetalert2";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
+// Use BootstrapVue
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+// Use VueSweetalert2
+Vue.use(VueSweetalert2);
+
+// Set route title
+router.beforeEach((to, from, next) => {
+  document.title = to.meta!.title || 'DUDU';
+  next();
+});
 
 Vue.config.productionTip = false
 
